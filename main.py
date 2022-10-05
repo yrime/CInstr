c05f3524d671c4199b775cc5760c13050771b37c
yrime <yrime@mail.ru>
2022-10-05 14:02:23 +0300

    fd

--- main.py
+++ main.py
@@ -5,12 +5,53 @@ filename = sys.argv[1]
 with open(filename) as file:
     data = file.read()
 
+basebloks = []
+funclist = []
+outputstr = ""
 def check_function(text):
+    global outputstr
     li = 0
     fi = 0
+    bo = False
+    baseline1 = -1
+    baseline2 = -1
+    linenum = -1
     lines = text.split('\n')
     for line in lines:
-        print(line)
+        linenum = linenum + 1
+        for c in line:
+            if bo:
+                if c == '{' and fi == 0:
+                    funclist.append(linenum)
+                    print('MACRO_FUNC')
+                elif c == ';' and fi != 0:
+                    print('MACRO_CALL')
+                else:
+                    bo = False
+            if c == '(':
+                li = li + 1
+                print(line, 'li', li, linenum)
+            elif c == ')':
+                li = li - 1
+                bo = True
+                print(line, 'li', li, linenum)
+            elif c == '{':
+                fi = fi + 1
+                if baseline1 == -1:
+                    baseline1 = linenum
+                else:
+                    baseline2 = linenum - 1
+                    basebloks.append((baseline1, baseline2))
+                    baseline1 = -1
+                print(line, 'fi', fi, linenum)
+            elif c == '}':
+                fi = fi - 1
+                if baseline1 != -1:
+                    baseline2 = linenum - 1
+                    basebloks.append((baseline1, baseline2))
+                    baseline1 = -1
+                print(line, 'fi', fi, linenum)
+        outputstr += (line + '\n')
 
 def nullstr_remover(text):
     lines = text.split('\n')
@@ -37,8 +78,13 @@ ou = comment_remover(data)
 oun = nullstr_remover(ou)
 check_function(oun)
 filename = sys.argv[1]
+
+print(basebloks, funclist)
+
+print(outputstr)
+
 with open(filename) as file:
     i = -1
     for line in file:
         i = i + 1
-        print(line.rstrip(), i)
+    #    print(line.rstrip(), i)
